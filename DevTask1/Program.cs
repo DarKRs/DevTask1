474048ba84bb4de360e07a8bf05ae6c8241fa3e9
DarKRs <centralstreet_24@mail.ru>
2022-01-26 20:29:02 +0600

    Начальные приготовления
    Readme, ignore, project

---
+++ DevTask1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DevTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
